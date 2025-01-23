fe77159a3d7ee6f6ccdf9a0bc2393e31b3418d91
Dinos <143280137+Dinos17@users.noreply.github.com>
2025-01-23 22:37:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from datetime import datetime, timedelta
 
 # ===== CONFIGURATION AND SETUP =====
 
-token = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 client_id = os.getenv("REDDIT_CLIENT_ID")
 client_secret = os.getenv("REDDIT_CLIENT_SECRET")
 user_agent = os.getenv("REDDIT_USER_AGENT")
